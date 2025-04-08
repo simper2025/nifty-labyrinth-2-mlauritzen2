883f87ec33ae72427fd7c892dabf2ea5b328db54
Michael Lauritzen <michael.lauritzen@students.snow.edu>
2025-04-08 09:20:52 -0600

    I did some of the extra credit

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,10 @@
 #include "Labyrinth.h"
 #include <iostream>
 #include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
 
 /* Change this constant to contain your name.
  *
@@ -15,35 +19,76 @@ const std::string kYourName = "Michael Lauritzen";
 const std::string kPathOutOfNormalMaze = "SSSWENWNWSWSNENESENNWWWS";
 const std::string kPathOutOfTwistyMaze = "NSENENNE.";
 
+//maze solver test
+string SolveMaze(MazeCell*, string,bool,bool,bool);
+
+
+bool hasPot = false;
+bool hasWand = false;
+bool hasBook = false;
+
+set<string>edges;
+
+auto contains = edges.find(MazeCell*) != bagOfHolding.end();
+
 int main() {
-    /* Generate the maze.
-     *
-     * Note: Don't set a breakpoint on this line. Otherwise, you'll see startLocation before
-     * it's been initialized.
-     */
-    MazeCell* startLocation = mazeFor(kYourName);
-    
-    /* Set a breakpoint here to explore your maze! */
-    if (isPathToFreedom(startLocation, kPathOutOfNormalMaze)) {
-        std::cout << "Congratulations! You've found a way out of your labyrinth." << std::endl;
-    } else {
-        std::cout << "Sorry, but you're still stuck in your labyrinth." << std::endl;
-    }
-    
-    
-    /* Generate the twisty maze.
-     *
-     * Note: Don't set a breakpoint on this line. Otherwise, you'll see twistyStartLocation before
-     * it's been initialized.
-     */
-    MazeCell* twistyStartLocation = twistyMazeFor(kYourName);
-    
-    /* Set a breakpoint here to explore your twisty maze! */
-    if (isPathToFreedom(twistyStartLocation, kPathOutOfTwistyMaze)) {
-        std::cout << "Congratulations! You've found a way out of your twisty labyrinth." << std::endl;
-    } else {
-        std::cout << "Sorry, but you're still stuck in your twisty labyrinth." << std::endl;
-    }
-    
-    return 0;
+	/* Generate the maze.
+	 *
+	 * Note: Don't set a breakpoint on this line. Otherwise, you'll see startLocation before
+	 * it's been initialized.
+	 */
+	MazeCell* startLocation = mazeFor(kYourName);
+
+	/* Set a breakpoint here to explore your maze! */
+	if (isPathToFreedom(startLocation, kPathOutOfNormalMaze)) {
+		std::cout << "Congratulations! You've found a way out of your labyrinth." << std::endl;
+	}
+	else {
+		std::cout << "Sorry, but you're still stuck in your labyrinth." << std::endl;
+	}
+
+
+	/* Generate the twisty maze.
+	 *
+	 * Note: Don't set a breakpoint on this line. Otherwise, you'll see twistyStartLocation before
+	 * it's been initialized.
+	 */
+	MazeCell* twistyStartLocation = twistyMazeFor(kYourName);
+
+	string path = SolveMaze(startLocation, path, hasPot, hasBook, hasWand);
+
+	/* Set a breakpoint here to explore your twisty maze! */
+	if (isPathToFreedom(twistyStartLocation, kPathOutOfTwistyMaze)) {
+		std::cout << "Congratulations! You've found a way out of your twisty labyrinth." << std::endl;
+	}
+	else {
+		std::cout << "Sorry, but you're still stuck in your twisty labyrinth." << std::endl;
+	}
+
+	return 0;
+}
+
+
+string SolveMaze(MazeCell* curr, string path, bool pot, bool book, bool wand)
+{
+
+	if (curr->whatsHere == Item::POTION)
+		pot = true;
+	if (curr->whatsHere == Item::SPELLBOOK)
+		book = true;
+	if (curr->whatsHere == Item::WAND)
+		wand = true;
+
+	if (pot && book && wand)
+		return path;
+
+	if (curr->north != nullptr)
+		return SolveMaze(curr->north, path + "N", pot, book, wand);
+	if (curr->south != nullptr)
+		return SolveMaze(curr->south, path + "S", pot, book, wand);
+	if (curr->east != nullptr)
+		return SolveMaze(curr->east, path + "E", pot, book, wand);
+	if (curr->west != nullptr)
+		return SolveMaze(curr->west, path + "W", pot, book, wand);
+
 }
