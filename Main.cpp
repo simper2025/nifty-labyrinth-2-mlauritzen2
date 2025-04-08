0e0187d34accaa6b8118bd8488c635064d3ed372
Michael Lauritzen <michael.lauritzen@students.snow.edu>
2025-04-08 08:36:39 -0600

    i sloved the maze

--- Main.cpp
+++ Main.cpp
@@ -9,11 +9,11 @@
  * do NOT edit the value of kYourName. Changing kYourName will change which
  * maze you get back, which might invalidate all your hard work!
  */
-const std::string kYourName = "TODO: Replace this string with your name.";
+const std::string kYourName = "Michael Lauritzen";
 
 /* Change these constants to contain the paths out of your mazes. */
-const std::string kPathOutOfNormalMaze = "TODO: Replace this string with your path out of the normal maze.";
-const std::string kPathOutOfTwistyMaze = "TODO: Replace this string with your path out of the twisty maze.";
+const std::string kPathOutOfNormalMaze = "SSSWENWNWSWSNENESENNWWWS";
+const std::string kPathOutOfTwistyMaze = "NSENENNE.";
 
 int main() {
     /* Generate the maze.
